b94d02b0c3b6f01ea37f2465098c3a3d3be7d19c
BerkP <45625829+BerkP@users.noreply.github.com>
2020-05-13 21:22:53 +0300

    Add files via upload

---
+++ server/src/main/java/cse222/group8/server/DailyExecutions.java
@@ -0,0 +1,19 @@
+package cse222.group8.server;
+
+public class DailyExecutions {
+	
+	System system;
+	
+	public DailyExecutions(System system) {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void resetDailyTasks() {
+		
+	}
+	
+	public void dropExpiredAdoptionRequests() {
+		
+	}
+
+}
---
+++ server/src/main/java/cse222/group8/server/JavalinServer.java
@@ -0,0 +1,11 @@
+package cse222.group8.server;
+
+public class JavalinServer implements Runnable {
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
