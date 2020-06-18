f4f693d328b04b65908700f164e63292a7fe7cd7
İsmail Tapan <ismltpn@gmail.com>
2020-06-18 14:31:22 +0300

    Created Home and Animals pages

--- desktop-client/src/main/java/cse222/group8/desktop/Main.java
+++ desktop-client/src/main/java/cse222/group8/desktop/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application{
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("./SelectShelterPage.fxml"));
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("./AnimalsPage.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 960, 540));
         primaryStage.show();
