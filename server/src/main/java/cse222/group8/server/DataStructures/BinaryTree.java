d2e9d90f25a839d9752c87be2a2369bb2da63517
SeymaCanbaz <64903287+SeymaCanbaz@users.noreply.github.com>
2020-07-04 17:05:00 +0300

    Update BinaryTree.java

--- server/src/main/java/cse222/group8/server/DataStructures/BinaryTree.java
+++ server/src/main/java/cse222/group8/server/DataStructures/BinaryTree.java
@@ -127,6 +127,16 @@ public class BinaryTree<E> implements Serializable {
 		return null;
 	}
 
+	public int size() {
+		return(size(root));
+	}
+	private int size(Node node) {
+		if (node == null) return(0);
+		else {
+			return(size(node.left) + 1 + size(node.right));
+		}
+	}
+
 	/**
 	 * Gets data.
 	 *
